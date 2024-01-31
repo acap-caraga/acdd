a7cfa637df798f7d9bd2c78fcdad361296d325e9
acapda13dev <acapda13.tutorials@gmail.com>
2024-01-31 15:47:56 +0800

    first commit

--- pages/api/attendance_view.js
+++ pages/api/attendance_view.js
@@ -4,6 +4,8 @@ const prisma = new PrismaClient();
 const handler = async (req, res) => {
     const pip = await prisma.$queryRaw`SELECT
         DATE_FORMAT(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p'), '%Y-%m-%d') AS Date,
+        MONTH(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p')) AS Month,
+        YEAR(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p')) AS Year,
         Name AS Name,
         MIN(CASE WHEN Status = 'C/In' AND TIME_FORMAT(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p'), '%H:%i') BETWEEN '04:00' AND '10:00' THEN TIME_FORMAT(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p'), '%h:%i %p') END) AS AM_In,
         MIN(CASE WHEN Status = 'Out Back' AND TIME_FORMAT(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p'), '%H:%i') BETWEEN '12:00' AND '12:30' THEN TIME_FORMAT(STR_TO_DATE(Date_Time, '%d/%m/%Y %h:%i:%s %p'), '%h:%i %p') END) AS AM_Out,
@@ -12,7 +14,7 @@ const handler = async (req, res) => {
     FROM
         attendance
     GROUP BY
-        Date, Name
+        Date, Month, Year, Name
     ORDER BY
         Date, Name;`;
 
